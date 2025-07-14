250741640974f05de8bc24d4631ba0b488343c11
pt7481 <paul.tongyoo@gmail.com>
2025-07-13 17:41:30 -0700

    Added data     input/output folders to s3 bucket

--- infrastructure/modules/s3/main.tf
+++ infrastructure/modules/s3/main.tf
@@ -2,3 +2,11 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
   force_destroy = true
 }
+
+resource "aws_s3_object" "prefixes" {
+  for_each = toset(["data/input/", "data/output/"])
+
+  bucket = aws_s3_bucket.bucket.id
+  key    = each.key
+  content = ""
+}
