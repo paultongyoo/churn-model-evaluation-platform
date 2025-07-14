7d6017c36e16e7e9b1c16c33cca7e1c5c7c4beed
pt7481 <paul.tongyoo@gmail.com>
2025-07-13 23:06:40 -0700

    Updated method signature to receive parameters from s3_to_prefect lambda

--- code/flows/churn_prediction_pipeline.py
+++ code/flows/churn_prediction_pipeline.py
@@ -5,17 +5,20 @@ It also includes task(s) to retrain the model if the model's churn prediction
 performance does not meet specified threshold.
 """
 
+import sys
+
 from prefect import flow
 
 
 @flow(name="churn_prediction_pipeline", log_prints=True)
-def churn_prediction_pipeline():
+def churn_prediction_pipeline(bucket: str, key: str):
     """
     A Prefect flow that orchestrates the churn prediction pipeline.
     It includes tasks for data ingestion, preprocessing, model training,
     evaluation, and potentially retraining the model based on performance.
     """
     print("Starting the Churn Prediction Pipeline...")
+    print(f"Processing data from bucket: {bucket}, key: {key}")
 
     # Here you would call other tasks to perform the steps of the pipeline
     # For example:
@@ -25,6 +28,12 @@ def churn_prediction_pipeline():
     # evaluate_model(model, preprocessed_data)
 
 
+# This allows the flow to be run directly for testing purposes
 if __name__ == "__main__":
-    # This allows the flow to be run directly for testing purposes
-    churn_prediction_pipeline()
+    if len(sys.argv) != 3:
+        print("Usage: python churn_prediction_pipeline.py <bucket> <key>")
+        sys.exit(1)
+    param1 = sys.argv[1]
+    param2 = sys.argv[2]
+    print(f"Running churn prediction pipeline with bucket: {param1}, key: {param2}")
+    churn_prediction_pipeline(bucket=param1, key=param2)
