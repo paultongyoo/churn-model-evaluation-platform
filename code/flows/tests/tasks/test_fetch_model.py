091748b268b6976e4cc3430efbca0687b495fd20
pt7481 <paul.tongyoo@gmail.com>
2025-07-16 09:10:33 -0700

    Updated test to prevent calling Prefect orchetration

--- code/flows/tests/tasks/test_fetch_model.py
+++ code/flows/tests/tasks/test_fetch_model.py
@@ -12,16 +12,19 @@ correctly retrieves a model from MLflow and handles errors appropriately."""
 
 
 class TestFetchModel(unittest.TestCase):
+    @patch("flows.churn_prediction_pipeline.get_run_logger")
     @patch("flows.churn_prediction_pipeline.mlflow")
-    def test_fetch_model_success(self, mock_mlflow):
+    def test_fetch_model_success(self, mock_mlflow, mock_logger):
         """
         Test that fetch_model retrieves the model correctly.
         """
+        mock_logger.return_value = MagicMock()
+
         mock_model = MagicMock()
         mock_model.input_example = {"feature1": "value1", "feature2": "value2"}
         mock_mlflow.pyfunc.load_model.return_value = mock_model
 
-        model = fetch_model("http://mlflow-server:5000", "test_model", "latest")
+        model = fetch_model.fn("http://mlflow-server:5000", "test_model", "latest")
 
         self.assertEqual(
             model.input_example, {"feature1": "value1", "feature2": "value2"}
